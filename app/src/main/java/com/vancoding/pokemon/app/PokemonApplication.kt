d225628b76c14225021296ead97b85ff8aa86a5c
Joshlanuevo <lanuevo68@gmail.com>
2025-02-25 15:55:46 +0800

    setup dependency injection

--- app/src/main/java/com/vancoding/pokemon/app/PokemonApplication.kt
+++ app/src/main/java/com/vancoding/pokemon/app/PokemonApplication.kt
@@ -1,6 +1,8 @@
 package com.vancoding.pokemon.app
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class PokemonApplication: Application() {
 }
--- app/src/main/java/com/vancoding/pokemon/di/NetworkModule.kt
+++ app/src/main/java/com/vancoding/pokemon/di/NetworkModule.kt
@@ -1,4 +1,56 @@
 package com.vancoding.pokemon.di
 
+import com.vancoding.pokemon.data.remote.api.PokeApiService
+import com.vancoding.pokemon.data.remote.config.ApiConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(ApiConfig.TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .readTimeout(ApiConfig.TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .writeTimeout(ApiConfig.TIMEOUT_SECONDS, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(ApiConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providePokeApiService(retrofit: Retrofit): PokeApiService {
+        return retrofit.create(PokeApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+    }
 }
