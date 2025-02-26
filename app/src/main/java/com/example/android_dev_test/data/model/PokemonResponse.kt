23028b37a0f31dce9d263722b14f91d9b63d461f
Erick Resurreccion <erick.taisondigital@gmail.com>
2025-02-26 21:01:48 +0800

    Added models, created ApiService and try test it in MainActivity

---
+++ app/src/main/java/com/example/android_dev_test/data/model/PokemonResponse.kt
@@ -0,0 +1,18 @@
+package com.example.android_dev_test.data.model
+
+data class PokemonResponse(
+    val count: Int, // Total number of Pokémon
+    val next: String?, // URL for the next page of results
+    val previous: String?, // URL for the previous page of results
+    val results: List<PokemonResult> // List of Pokémon
+)
+
+data class PokemonResult(
+    val name: String, // Pokémon name
+    val url: String // URL to fetch details
+)
+//data class PokemonResponse(
+//    val id: Int,
+//    val name: String,
+//    val url: String,
+//)
