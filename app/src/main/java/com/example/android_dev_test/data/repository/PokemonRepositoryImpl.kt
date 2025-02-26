6eef3ec3f87fee22aa7e7d826d1c42f8d7d4a356
Erick Resurreccion <erick.taisondigital@gmail.com>
2025-02-27 03:35:17 +0800

    Added Offline support using Room database

--- app/src/main/java/com/example/android_dev_test/data/repository/PokemonRepositoryImpl.kt
+++ app/src/main/java/com/example/android_dev_test/data/repository/PokemonRepositoryImpl.kt
@@ -1,11 +1,17 @@
 package com.example.android_dev_test.data.repository
 
+import com.example.android_dev_test.data.local.PokemonDatabase
+import com.example.android_dev_test.data.local.PokemonDetailsEntity
+import com.example.android_dev_test.data.local.PokemonEntity
 import com.example.android_dev_test.data.model.PokemonDetailsResponse
 import com.example.android_dev_test.data.model.PokemonResponse
 import com.example.android_dev_test.data.network.ApiService
 import javax.inject.Inject
 
-class PokemonRepositoryImpl @Inject constructor(private val apiService: ApiService) :
+class PokemonRepositoryImpl @Inject constructor(
+    private val apiService: ApiService,
+    private val database: PokemonDatabase
+) :
     PokemonRepository {
 
     override suspend fun getPokemonList(limit: Int, offset: Int): PokemonResponse {
@@ -20,5 +26,14 @@ class PokemonRepositoryImpl @Inject constructor(private val apiService: ApiServi
 
     override suspend fun getPokemonDetails(name: String): PokemonDetailsResponse {
         return apiService.getPokemonDetails(name)
+    }
+
+    //  methods to get data from the database
+    suspend fun getPokemonListFromDatabase(): List<PokemonEntity> {
+        return database.pokemonDao().getPokemonList()
+    }
+
+    suspend fun getPokemonDetailsFromDatabase(name: String): PokemonDetailsEntity? {
+        return database.pokemonDao().getPokemonDetails(name)
     }
 }
--- app/src/main/java/com/example/android_dev_test/di/NetworkModule.kt
+++ app/src/main/java/com/example/android_dev_test/di/NetworkModule.kt
@@ -1,5 +1,8 @@
 package com.example.android_dev_test.di
 
+import android.content.Context
+import com.example.android_dev_test.data.local.PokemonDao
+import com.example.android_dev_test.data.local.PokemonDatabase
 import com.example.android_dev_test.data.network.ApiService
 import com.example.android_dev_test.data.network.PokemonRetrofit
 import com.example.android_dev_test.data.repository.PokemonRepository
@@ -7,6 +10,7 @@ import com.example.android_dev_test.data.repository.PokemonRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -22,8 +26,17 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    fun providePokemonRepository(pokemonService: ApiService): PokemonRepository {
-        return PokemonRepositoryImpl(pokemonService)
+    fun providePokemonRepository(
+        pokemonService: ApiService,
+        pokemonDatabase: PokemonDatabase
+    ): PokemonRepository {
+        return PokemonRepositoryImpl(pokemonService, pokemonDatabase)
+    }
+
+    @Provides
+    @Singleton
+    fun providePokemonDatabase(@ApplicationContext context: Context): PokemonDatabase {
+        return PokemonDatabase.getDatabase(context)
     }
 
 }
